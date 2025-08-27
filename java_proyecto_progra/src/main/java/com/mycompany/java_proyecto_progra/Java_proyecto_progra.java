25635d98f25935d7487484c6c3c0fd1ab22be365
Your Name <you@example.com>
2025-08-27 18:33:02 -0400

    commit prueba

---
+++ java_proyecto_progra/src/main/java/com/mycompany/java_proyecto_progra/Java_proyecto_progra.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java_proyecto_progra;
+
+/**
+ *
+ * @author mati
+ */
+public class Java_proyecto_progra {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+//hola proabndop
